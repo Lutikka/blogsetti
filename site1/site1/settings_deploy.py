fb7c2a9774863e7f185be4cf72feae482c1c250b
Lutikka <erkki.sinisalo@gmail.com>
2016-05-30 05:43:18 +0300

    some version before deployment

--- site1/site1/settings_deploy.py
+++ site1/site1/settings_deploy.py
@@ -24,11 +24,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secrets.SECRET_KEY_1
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # bug false = statics are missing like style.css etc.
+DEBUG = False # bug false = statics are missing like style.css etc.
 
 #should probably also include server external ip and domain.
 ALLOWED_HOSTS = ['localhost', '127.0.0.1'
 ]
+
+DEFAULT_FROM_EMAIL = 'noreply@localhost'
+SERVER_EMAIL = 'errors@localhost'
 
 
 
